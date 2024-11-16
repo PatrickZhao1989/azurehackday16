3146787a7514743262dbc6282ff246dbe6d50778
Patrick Zhao <patrick.zhao1989@outlook.com>
2024-11-16 11:59:16 +1100

    Initial commit - scaffold the frontend app

---
+++ frontend/src/App.tsx
@@ -0,0 +1,15 @@
+import './App.css'
+
+function App() {
+    const name = "Patrick";
+  // const [count, setCount] = useState(0)
+
+  return (
+    <>
+    <h1>Hello World!!! {name}</h1>
+        
+    </>
+  )
+}
+
+export default App
