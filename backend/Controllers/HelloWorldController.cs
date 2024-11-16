3035d24577626fdc1fba472406008d828ccd9add
Patrick Zhao <patrick.zhao1989@outlook.com>
2024-11-16 14:49:25 +1100

    Created backend .net web api

---
+++ backend/Controllers/HelloWorldController.cs
@@ -0,0 +1,14 @@
+using Microsoft.AspNetCore.Mvc;
+
+[Route("api/[controller]")]
+[ApiController]
+public class HelloWorldController : ControllerBase
+{
+
+    [HttpGet]
+    public ActionResult<string> HelloWorld()
+    {
+        var name = "Patrick";
+        return $"Hello world {name}";
+    }
+}
